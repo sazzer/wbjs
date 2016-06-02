f4e53a71b525223dd23da84c6b159f55a92717af
Graham Cox <graham@grahamcox.co.uk>
2016-06-02 19:23:59 +0100

    Thrown away all of the Hapi code

--- src/server/main.js
+++
@@ -1,9 +0,0 @@
-import { startServer } from './server/index.js'
-
-startServer()
-  .then((server) => {
-    console.log('Started server');
-  }).catch((err) => {
-    console.log('Failed to start server');
-    console.log(err);
-  });

be529e750b660a9959ef1ace74a2c14dd11fa287
Graham Cox <graham@grahamcox.co.uk>
2016-06-03 08:03:44 +0100

    Use the ResultSet when loading worlds

--- src/server/worlds/finder.spec.js
+++ src/server/worlds/finder.spec.js
@@ -1,12 +1,16 @@
 import { expect } from 'chai';
 import { findAllWorlds, findWorldById } from './finder';
+import { ResultSet } from '../service/resultset';
 
 describe('worlds/finder', function() {
   describe('findAllWorlds', function() {
     describe('No filters, hard-coded data', function() {
       const results = findAllWorlds();
+      it('Returns a ResultSet', function() {
+        return expect(results).to.eventually.be.instanceOf(ResultSet);
+      });
       it('Returns the correct values', function() {
-        return expect(results).to.eventually.have.length(1);
+        return expect(results).to.eventually.have.property('results').with.size(1);
       });
     })
   })

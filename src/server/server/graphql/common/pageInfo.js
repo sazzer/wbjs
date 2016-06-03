3a401af54852061568d69535678a7459f22c5aa2
Graham Cox <graham@grahamcox.co.uk>
2016-06-03 11:03:38 +0100

    Ability to translate a ResultSet into a PageInfo representation

--- src/server/server/graphql/common/pageInfo.js
+++ src/server/server/graphql/common/pageInfo.js
@@ -17,3 +17,21 @@ export const PageInfo = new GraphQLObjectType({
     count: {type: new GraphQLNonNull(GraphQLInt)}
   }
 });
+
+/**
+ * Translator to take a ResultSet object and translate it into a PageInfo data structure
+ * @param {ResultSet} resultset The result set to translate
+ * @return {Object} the representation of the PageInfo details for this resultset
+ */
+export function pageInfoTranslator(resultset) {
+  const hasPreviousPage = (resultset.offset > 0);
+  const lastOffset = resultset.offset + resultset.results.size;
+  const hasNextPage = lastOffset < resultset.count;
+
+  return {
+    hasNextPage,
+    hasPreviousPage,
+    pageOffset: resultset.offset,
+    count: resultset.count
+  };
+}

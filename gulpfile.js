ee256034bf15f3c3c84557d5155612c8be987d75
Graham Cox <graham@grahamcox.co.uk>
2016-06-08 08:17:53 +0100

    Gulp Load Plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,35 @@
 var gulp = require('gulp'),
-  babel = require('gulp-babel'),
-  config = require('config'),
-  env = require('gulp-env'),
-  eslint = require('gulp-eslint'),
-  exec = require('child_process').exec,
-  istanbul = require('gulp-istanbul'),
-  jscpd = require('gulp-jscpd'),
-  mocha = require('gulp-mocha'),
+  plugins = require('gulp-load-plugins')({
+    DEBUG: true
+  }),
   remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul'),
-  sourcemaps = require('gulp-sourcemaps');
+  exec = require('child_process').exec;
 
 function buildLint(target) {
   return function() {
     return gulp.src(['src/' + target + '/**/*.js'])
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+      .pipe(plugins.eslint())
+      .pipe(plugins.eslint.format())
+      .pipe(plugins.eslint.failAfterError());
   };
 }
 
 function buildJscpd(target) {
   return function() {
     return gulp.src(['src/' + target + '/**/*.js'])
-      .pipe(jscpd());
+      .pipe(plugins.jscpd());
   }
 }
 
 function buildBabel(target) {
   return function() {
     return gulp.src(['src/' + target + '/**/*.js'])
-      .pipe(sourcemaps.init())
-      .pipe(babel({
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.babel({
         plugins: ['rewire', 'typecheck', 'syntax-flow', 'transform-flow-strip-types'],
         presets: ['es2015']
       }))
-      .pipe(sourcemaps.write('.'))
+      .pipe(plugins.sourcemaps.write('.'))
       .pipe(gulp.dest('target/' + target));
   }
 }
@@ -61,20 +56,20 @@ function migrate(dir, cb) {
 
 gulp.task('pre-unit-test', ['build'], function() {
   return gulp.src(['target/server/**/*.js'])
-    .pipe(istanbul({
+    .pipe(plugins.istanbul({
       includeUntested: true,
     }))
-    .pipe(istanbul.hookRequire());
+    .pipe(plugins.istanbul.hookRequire());
 });
 
 gulp.task('unit-test', ['pre-unit-test'], function() {
   return gulp.src(['target/server/**/*.spec.js'])
-    .pipe(mocha({
+    .pipe(plugins.mocha({
       ui: 'bdd',
       reporter: 'spec',
       require: ['./target/server/test-helper']
     }))
-    .pipe(istanbul.writeReports({
+    .pipe(plugins.istanbul.writeReports({
       dir: './target/coverage/server',
       reporters: ['lcovonly', 'json']
     }));

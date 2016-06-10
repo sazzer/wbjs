1c03be23508ef51322c9d5a24e210adeb096d95c
Graham Cox <graham@grahamcox.co.uk>
2016-06-10 16:48:50 +0100

    Removed the old Mocha integration tests

--- src/integration/test-helper.js
+++
@@ -1,9 +0,0 @@
-require('source-map-support').install();
-
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import chaiImmutable from 'chai-immutable';
-
-chai.should();
-chai.use(chaiImmutable);
-chai.use(chaiAsPromised);

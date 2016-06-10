d7efa53717765f011c7d6362b11c540897b6c6e9
Graham Cox <graham@grahamcox.co.uk>
2016-06-10 09:27:50 +0100

    Start and stop the server around the cucumber tests

--- src/cucumber/support/server.js
+++ src/cucumber/support/server.js
@@ -1,10 +1,55 @@
+import freeport from 'freeport';
+import { startServer as startTestServer } from '../../server/server/index.js'
+
+/** The actual server that we're testing */
+let serverPromise;
+
+/** The URI referring to the server */
+let serverUri;
+
 module.exports = function() {
   this.registerHandler('BeforeFeatures', function(event, callback) {
     console.log('Starting the server');
-    callback();
+
+    serverPromise = new Promise((resolve, reject) => {
+      freeport((err,port) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(port);
+        }
+      })
+    })
+    .then((port) => {
+      return startTestServer(port);
+    });
+
+    serverPromise.then(function(server) {
+      serverUri = server.info.uri;
+      console.log(`Started server on: ${serverUri}`);
+      callback();
+    })
+    .catch((err) => {
+      console.log('Failed to start server');
+      console.log(err);
+      callback(err);
+    });
   });
+
   this.registerHandler('AfterFeatures', function(event, callback) {
     console.log('Stopping the server');
-    callback();
+
+    serverPromise.then((server) => {
+      return server.stop();
+    })
+    .then(() => {
+      console.log('Stopped server');
+      callback();
+    })
+    .catch((err) => {
+      console.log('Failed to stop server');
+      console.log(err);
+      callback(err);
+    });
   });
 }
